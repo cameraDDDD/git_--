7d3e514998e60189807d101d2f8ce9e15f694a62
cameraDD <kevin1511249859@outlook.com>
2024-09-22 19:58:54 +0800

    ATM复活

--- 记账.py
+++ 记账.py
@@ -6,27 +6,29 @@ wallet = Path("V:/camera/文档/钱包.json")#获取钱包路径
 #dic = json.dumps(py_dict)
 #wallet.write_text(dic)
 today_date=datetime.today().date()#获取日期
+str_date=today_date.strftime("%Y/%m/%d")
 json_dict = wallet.read_text()#获取json字典
 py_dict = json.loads(json_dict)#转换成python字典
 amount = py_dict.get("amount","字典有误")#获取余额
-#with open("C:/Users/Kevin/Desktop/账本.txt","a",encoding="UTF-8") as f:
-    #f.write(f"\n------{today_date}------\n")
-QUIT = False
-while QUIT == False:
-    expend_th = input("支出项目:")
-    if expend_th == "q":
-        break
-    try:    
-        expend_m = float(input("支出金额:"))
-        py_dict[expend_th]=expend_m
-        amount -= expend_m
-        py_dict["amount"]=amount
+if py_dict.get(str_date)==None:
+    py_dict[str_date]={}
 
-    except ValueError:
-        print("金额必须为数字")
-    finally:
-        print(f"余额为{amount}")
-        
+while True:
+    print("--------记账--------")
+    print("退出 请按'0'")
+    print("支出 请按'1'")
+    choice=input("请输入:")
+    if choice=='0':
+        break
+    if choice=='1':
+        expend_th=input("支出项目:")
+        try:
+            expend_m=float(input("支出金额:"))
+            py_dict["amount"]=amount-expend_m
+            py_dict[str_date][expend_th]=expend_m
+            print(f"余额为{amount}元")
+        except ValueError:
+            print("输入数字md")
 json_dict = json.dumps(py_dict,ensure_ascii=False)
 wallet.write_text(json_dict)
     
